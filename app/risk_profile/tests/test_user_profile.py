e0cd11157f26573c106e2e9fd0648eab9db038e0
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-30 10:55:42 -0300

    add rest_framework, endpoint red tdd

--- app/risk_profile/tests/test_user_profile.py
+++ app/risk_profile/tests/test_user_profile.py
@@ -1,13 +1,44 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+RISK_ENDPOINT = reverse('risk:create')
 
 
 class UserProfileTest(TestCase):
 
+    def setUp(self):
+        self.client = APIClient()
+
     def test_empty_payload(self):
-        pass
+        payload = {}
+        res = self.client.post(RISK_ENDPOINT, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_full_payload(self):
-        pass
+        payload = {
+            "age": 35,
+            "dependents": 2,
+            "house": {"ownership_status": "owned"},
+            "income": 0,
+            "marital_status": "married",
+            "risk_questions": [0, 1, 0],
+            "vehicle": {"year": 2018}
+        }
+        res = self.client.post(RISK_ENDPOINT, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
     def test_invalid_payload(self):
-        pass
+        payload = {
+            "age": 'thirty-five',
+            "dependents": -1,
+            "house": {"ownership_status": "borrowed"},
+            "income": -100,
+            "marital_status": "complicated",
+            "risk_questions": [0, 1, 0],
+            "vehicle": {"year": 2198}
+        }
+        res = self.client.post(RISK_ENDPOINT, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
